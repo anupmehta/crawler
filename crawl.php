9ceed12687e521ae83de471618442bab38c0cd50
root <root@cdn31.(none)>
2014-01-21 12:10:50 +0530

    Code updated

--- crawl.php
+++ crawl.php
@@ -16,8 +16,7 @@ class Crawler
 		$opts = array(
 		  'http'=>array(
 			'method'=>"GET",
-			'header'=>"Accept-language: en\r\n" .
-					  "Cookie: foo=bar\r\n"
+			'header'=>"Accept-language: en\r\n" 
 		  )
 		);
 
@@ -27,6 +26,23 @@ class Crawler
 		$file = file_get_contents($url, false, $context);
 		return $file;
     }
+    
+    //get Data from curl
+    function getDataFromCurl($url){
+	
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+		$file = curl_exec($ch);
+		curl_close($ch);
+
+		return $file;
+	}
+
  
    public function get($type)
     {
@@ -54,6 +70,18 @@ class Crawler
         return !empty($links[1]) ? $links[1] : FALSE;
       }
     }
+
+    function _get_title() {
+        if (!empty($this->markup))
+        { 
+		preg_match('/<title>(.*)<\/title>/siU', $this->markup, $title);
+
+        $title = preg_replace('/\s+/', ' ', $title[1]);
+        $title = trim($title);
+        return $title;
+    }
+}
+
 }  // End of Crawler class
  
 if(isset($_POST['url']) && $_POST['url'] != '')
@@ -74,8 +102,11 @@ if(!empty($links))
     {
       if($link[0] == "'") $link = substr($link,1,-1);
       if($link[0] == "/") $link = $_POST['url'].$link;
+      
      $etst[$link] =  makeCrawl($link);//call function recursively to get all hyperlinks of website pages
+     $etst[$link]['title'] = $title;
     }
+
 	echo "<pre>";print_r($etst);die;
 	foreach($etst as $key=>$values):
 		//echo "<h3>Page Url: ".$key."</h3><br>";
@@ -88,70 +119,35 @@ if(!empty($links))
 function makeCrawl($url){
 $images = null;
 $links = null;
+
 //We must enter http:// or https:// before the url, if it does not, then we check here
 //and write http if needed.
 if(substr($url, 0, 4) != 'http') $url = 'http://'.$url;
+
 //Create an object of class Crawler.
 $crawl = new Crawler($url);
+
 //Call the function get() with argument "images"
 if(is_array($crawl->get('images'))){
-$images = array_unique($crawl->get('images'));
+	$images = array_unique($crawl->get('images'));
 }
+//Call the function get() with argument "hyperlinks"
 if(is_array($crawl->get('links'))){
-$links  = array_unique($crawl->get('links'));
+	$links  = array_unique($crawl->get('links'));
 }
 
+//Call the function get() with argument "title"
+if($crawl->get('title')){
+	$title= $crawl->get('title');
+}
 
+//Creating data Array
+$dataArray['title']= $title;
 $dataArray['images'] = $images;
 $dataArray['hyperlinks']= $links;
-
-
-//Call the function get() with argument "links"
-/*
-$i = 0;
-echo "<table cellpadding='5'>";
-echo "<tr><td id='title'>IMAGE LINKS</td></tr>";
-//Here we check if array $images is empty or not. If it empty then we just pass the control to
-//the else condition.
-if(!empty($images))
-{
-    //Here we print the image links
-    foreach($images as $img)
-    {
-      if($i%2 == 0) $style = "style='background-color:#cccccc;'";
-      else $style="style='background-color:#eeeeee;'";
-      if($img[0] == "'") $img = substr($img,1,-1);
-      echo "<tr><td ".$style.">".$img."</td></tr>";
-      $i++;
-    }
-}
-else echo "<tr><td>No Image!</td></tr>";
-echo "</table>";
- 
-$j = 0;
-echo "<table cellpadding='5'>";
-echo "<tr><td id='title'>HYPERLINKS</td></tr>";
-//Here we check if array $links is empty or not. If it empty then we just pass the control to
-//the else condition.
-if(!empty($links))
-{
-    // print the hyperlinks
-    foreach($links as $link)
-    {
-      if($j%2 == 0) $style = "style='background-color:#cccccc;'";
-      else $style="style='background-color:#eeeeee;'";
-      if($link[0] == "'") $link = substr($link,1,-1);
-      if($link[0] == "/") $link = $_POST['url'].$link;
-      echo "<tr><td ".$style.">".$link."</td></tr>";
-      $j++;
-    }
-}
-else echo "<tr><td>No Hyperlink!</td></tr>";
-echo "</table>";
-*/
 
 return $dataArray;
 }
 
-
+	
 ?>
